e60150a00e01f3cb2a91cb316d311bb78e382baf
Kavya Pant <kavyapant@ymail.com>
2021-09-08 20:00:35 +0530

    create model to store data fetched from API

--- src/main/java/dev/project/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/dev/project/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,5 +1,6 @@
 package dev.project.coronavirustracker.services;
 
+import dev.project.coronavirustracker.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,21 +13,43 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service //Tells Spring Create an instance of this class when application starts running
 public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private List<LocationStats> allStats= new ArrayList<>();
+
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
     @PostConstruct //Tells Spring to run this method once class has been instantiated
     @Scheduled(cron="* * 6 * * *",zone="GMT")// cron uses servers local time zone can be changed using zone attribute
     public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats= new ArrayList<>();
         HttpClient client= HttpClient.newHttpClient();
         HttpRequest request=HttpRequest.newBuilder().uri(URI.create(VIRUS_DATA_URL)).build();
         HttpResponse<String> httpResponse=client.send(request, HttpResponse.BodyHandlers.ofString());
         StringReader in = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
         for (CSVRecord record : records) {
-            System.out.println(record.get("Province/State"));
+            LocationStats locationStat=new LocationStats();
+            String state=record.get("Province/State");
+            if(state.isEmpty())
+            {
+                state="-";
+            }
+            locationStat.setState(state);
+            locationStat.setCountry(record.get("Country/Region"));
+            int latestCases=Integer.parseInt(record.get(record.size()-1));
+            int previousDayCases=Integer.parseInt(record.get(record.size()-2));
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromPrevDay(latestCases-previousDayCases);
+            newStats.add(locationStat);
         }
+        this.allStats=newStats;
     }
 }
